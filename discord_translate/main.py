192c9e0c1ba396fa4906b5cb41e3cfb1de37bcb7
red <red@kalab.sk>
2023-11-30 15:54:08 +0100

    add /translate command, more logs

--- discord_translate/main.py
+++ discord_translate/main.py
@@ -3,7 +3,7 @@
 import asyncio
 import io
 import os
-from typing import List
+from typing import List, Optional, TypedDict
 import discord
 from discord.ext import commands
 import aiohttp
@@ -54,11 +54,46 @@ async def ocr(message: discord.Message) -> List[str]:
     return ocrd
 
 
+class DeepLTranslation(TypedDict):
+    detected_source_language: str
+    text: str
+
+
+class DeepLResponse(TypedDict):
+    translations: List[DeepLTranslation]
+
+
+async def do_translation(text: List[str], lang: str) -> List[DeepLTranslation]:
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api-free.deepl.com/v2/translate",
+                params={
+                    "auth_key": os.environ["DEEPL_KEY"],
+                    "target_lang": lang,
+                    "text": text,
+                },
+            ) as response:
+                data = await response.json()
+                translated_text = data["translations"]
+                print(f"Translated {text} - {translated_text}")
+
+    except Exception as e:
+        print(e)
+        raise e
+    else:
+        return translated_text
+
+
 @discord.app_commands.user_install()
 @discord.app_commands.allow_contexts(guilds=True, private_channels=True)
 @discord.app_commands.context_menu(name="Translate")
-async def translate(interaction: discord.Interaction, message: discord.Message):
+async def translate_msg(interaction: discord.Interaction, message: discord.Message):
     """Translate a message"""
+
+    print(
+        f"Translating a message from {message.author.name} for {interaction.user.name}"
+    )
 
     preferred_language = LANGUAGE_PREFERENCES.get(interaction.user.id, "en")
 
@@ -74,19 +109,7 @@ async def translate(interaction: discord.Interaction, message: discord.Message):
     image_count = len(to_translate) - image_count
 
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                "https://api-free.deepl.com/v2/translate",
-                params={
-                    "auth_key": os.environ["DEEPL_KEY"],
-                    "target_lang": preferred_language,
-                    "text": to_translate,
-                },
-            ) as response:
-                data = await response.json()
-                print(data)
-                translated_text = (data)["translations"]
-
+        translated_text = await do_translation(to_translate, preferred_language)
     except Exception as e:
         await interaction.response.send_message(
             f"Failed to translate :c", ephemeral=True
@@ -107,7 +130,7 @@ async def translate(interaction: discord.Interaction, message: discord.Message):
         await interaction.response.send_message(resp, ephemeral=True)
 
 
-bot.tree.add_command(translate)
+bot.tree.add_command(translate_msg)
 
 
 @discord.app_commands.user_install()
@@ -116,6 +139,7 @@ bot.tree.add_command(translate)
 async def prefer_language(interaction, language: str):
     """Set a preferred language for translation."""
     LANGUAGE_PREFERENCES[interaction.user.id] = language
+
     await interaction.response.send_message(
         f"Set preferred language to {language}", ephemeral=True
     )
@@ -123,5 +147,33 @@ async def prefer_language(interaction, language: str):
 
 bot.tree.add_command(prefer_language)
 
+
+@discord.app_commands.user_install()
+@discord.app_commands.allow_contexts(guilds=True, dms=True, private_channels=True)
+@discord.app_commands.describe(
+    text="The text to translate",
+    language="Language to translate to",
+)
+@discord.app_commands.command()
+async def translate(interaction, text: str, language: Optional[str] = None):
+    """Set a preferred language for translation."""
+
+    preferred_language = language or LANGUAGE_PREFERENCES.get(interaction.user.id, "en")
+
+    try:
+        translated_text = await do_translation([text], preferred_language)
+    except Exception as e:
+        await interaction.response.send_message(
+            f"Failed to translate :c", ephemeral=True
+        )
+        print(e)
+    else:
+        await interaction.response.send_message(
+            f"Translated message ({preferred_language}): `{translated_text[0]['text']}`",
+            ephemeral=True,
+        )
+
+
+bot.tree.add_command(translate)
 
 bot.run(os.environ["DISCORD_TOKEN"])
